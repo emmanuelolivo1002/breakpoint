6f17daf276c168bf71d0be920f9109db0b5ec215
Emmanuel Olivo <emmanuel@mmacbook-con-dos-emes.local>
2018-02-20 10:35:32 -0600

    Add DataService to communicate with Firebase Database

---
+++ breakpoint/breakpoint/Services/DataService.swift
@@ -0,0 +1,47 @@
+//
+//  DataService.swift
+//  breakpoint
+//
+//  Created by Emmanuel Olivo on 20/02/18.
+//  Copyright © 2018 Con Dos Emes. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+// Base URL for Firebase Database
+let DB_BASE = Database.database().reference()
+
+class DataService {
+    
+    static let instance = DataService()
+    
+    private var _REF_BASE = DB_BASE
+    private var _REF_USERS = DB_BASE.child("users")
+    private var _REF_GROUPS = DB_BASE.child("groups")
+    private var _REF_FEED = DB_BASE.child("feed")
+    
+    
+    var REF_BASE : DatabaseReference {
+        return _REF_BASE
+    }
+    
+    var REF_USERS : DatabaseReference {
+        return _REF_USERS
+    }
+    
+    var REF_GROUPS : DatabaseReference {
+        return _REF_GROUPS
+    }
+    
+    var REF_FEED : DatabaseReference {
+        return _REF_FEED
+    }
+    
+    // Function to create new users in Database
+    func createFirebaseUser(uid: String, userData: Dictionary<String, Any>) {
+        REF_USERS.child(uid).updateChildValues(userData)
+    }
+    
+    
+}
